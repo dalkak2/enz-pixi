d70ef7a6e85943898652c36b867f88ba0e5f33c8
Gnlow <gnlow@protonmail.com>
2023-12-05 15:20:44 +0900

    util: list blocks!
    - value_of_index_from_list
    - add_value_to_list
    - remove_value_from_list
    - insert_value_to_list
    - change_value_list_index
    - length_of_list
    - is_included_in_list

--- src/Entry.ts
+++ src/Entry.ts
@@ -27,7 +27,7 @@ export class Entry {
     renderer?: Renderer
     events: Record<string, (() => void)[]>
     scenes: Record<string, Container> = {}
-    variables: Record<string, string | number> = {}
+    variables: Record<string, string | number | (string | number)[]> = {}
     textures: Record<string, Texture> = {}
     objects: Record<string, EntrySprite> = {}
 
@@ -57,10 +57,10 @@ export class Entry {
     async init(parent: HTMLElement) {
         this.variables = Object.fromEntries(
             this.project.variables.map(
-                ({id, value}) => {
+                ({id, value, array}) => {
                     return [
                         id,
-                        value,
+                        array?.map(({data}) => data) || value,
                     ]
                 }
             )
@@ -460,5 +460,55 @@ export class Entry {
     }
     set_variable(id: string, value: string | number) {
         this.variables[id] = value
+    }
+    value_of_index_from_list(id: string, i: number) {
+        if (!Array.isArray(this.variables[id])) {
+            throw new Error(`${id} is not array`)
+        }
+        return (this.variables[id] as (string | number)[])[i - 1]
+    }
+    add_value_to_list(value: string | number, id: string) {
+        if (!Array.isArray(this.variables[id])) {
+            throw new Error(`${id} is not array`)
+        }
+        return (this.variables[id] as (string | number)[]).push(value)
+    }
+    remove_value_from_list(i: number, id: string) {
+        if (!Array.isArray(this.variables[id])) {
+            throw new Error(`${id} is not array`)
+        }
+        (this.variables[id] as (string | number)[]).splice(i - 1, 1)
+    }
+    insert_value_to_list(
+        value: string | number,
+        id: string,
+        i: number,
+    ) {
+        if (!Array.isArray(this.variables[id])) {
+            throw new Error(`${id} is not array`)
+        }
+        (this.variables[id] as (string | number)[]).splice(i - 1, 0, value)
+    }
+    change_value_list_index(
+        id: string,
+        i: number,
+        value: string | number,
+    ) {
+        if (!Array.isArray(this.variables[id])) {
+            throw new Error(`${id} is not array`)
+        }
+        (this.variables[id] as (string | number)[])[i - 1] = value
+    }
+    length_of_list(id: string) {
+        if (!Array.isArray(this.variables[id])) {
+            throw new Error(`${id} is not array`)
+        }
+        return (this.variables[id] as (string | number)[]).length
+    }
+    is_included_in_list(id: string, value: string | number) {
+        if (!Array.isArray(this.variables[id])) {
+            throw new Error(`${id} is not array`)
+        }
+        return (this.variables[id] as (string | number)[]).includes(value)
     }
 }
