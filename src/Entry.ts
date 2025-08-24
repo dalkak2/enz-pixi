b8e2f04e0cf34273cc69be6d02ddf38ed6e25e08
Gnlow <gnlow@pm.me>
2025-08-24 12:41:43 +0900

    refactor: simpler module import

--- src/Entry.ts
+++ src/Entry.ts
@@ -1,22 +1,28 @@
-import {
-    Start,
-    Flow,
-    Moving,
-    Looks,
-    Brush,
-} from "./block/mod.ts"
+import * as Block from "./block/mod.ts"
 import { Module } from "./Module.ts"
 
 // deno-lint-ignore no-explicit-any
 type Args<C extends { new(...args: any): any }> =
-C extends { new(...args: infer Args): infer _ }
-    ? Args
-    : never
+    C extends { new(...args: infer Args): infer _ }
+        ? Args
+        : never
 
-export interface Entry extends
-    Start,
-    Flow
-{
+type UnionToIntersection<T> =
+    (
+        T extends infer _ ? (x: T) => void : never
+    ) extends (x: infer R) => void
+        ? R
+        : never
+
+type StdModule = UnionToIntersection<
+    (typeof Block)[
+        keyof typeof Block
+    ] extends { new(...args: infer _): infer I }
+        ? I
+        : never
+>
+
+type Entry = StdModule & {
     modules: (typeof Module)[]
 }
 
@@ -26,12 +32,7 @@ export const Entry = function (this: Entry, ...args: Args<typeof Module>) {
         new Module(...args),
     )
     this.modules = [
-        Start,
-        Flow,
-        Moving,
-        Looks,
-        Brush,
-
+        ...Object.values(Block),
         Module,
     ]
     this.modules.forEach(module => {
