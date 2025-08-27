1241844bf5f3ec50d21a7279cc382f7f4f976997
Gnlow <gnlow@pm.me>
2025-08-27 16:35:07 +0900

    fix: brush overhaul
    fix #28

--- src/obj/EntryBrush.ts
+++ src/obj/EntryBrush.ts
@@ -1,18 +1,15 @@
-// deno-lint-ignore-file no-self-assign
-
 import { EntryContainer } from "./EntryContainer.ts"
 import {
     Graphics,
-    Color,
     type Container,
 } from "../../deps/pixi.ts"
 import type { Module } from "../Module.ts"
 
 export abstract class EntryBrush extends EntryContainer {
     protected override dataUpdate(sprite: this): void {
-        sprite._strokeColor = this._strokeColor
-        sprite._strokeThickness = this._strokeThickness
-        sprite._brushTransparency = this._brushTransparency
+        sprite.strokeColor = this.strokeColor
+        sprite.strokeThickness = this.strokeThickness
+        sprite.brushTransparency = this.brushTransparency
     }
 
     hasStrokeBrush = false
@@ -21,16 +18,17 @@ export abstract class EntryBrush extends EntryContainer {
     getStrokeBrush(onGraphicsInit?: (graphics: Graphics) => void) {
         if (!this._strokeBrush) {
             this._strokeBrush = new Graphics()
-            this.strokeColor = this.strokeColor
-            this.strokeThickness = this.strokeThickness
-            this.brushTransparency = this.brushTransparency
 
             this._lineListener = () => {
                 this._strokeBrush!.lineTo(
                     this.pixiSprite.x,
                     this.pixiSprite.y,
                 )
-                this._strokeBrush!.stroke()
+                this._strokeBrush!.stroke({
+                    width: this.strokeThickness,
+                    color: this.strokeColor,
+                    alpha: 1 - this.brushTransparency / 100,
+                })
             }
             onGraphicsInit?.(this._strokeBrush)
         }
@@ -40,41 +38,10 @@ export abstract class EntryBrush extends EntryContainer {
         }
     }
 
-    _strokeColor?: string
-    get strokeColor() {
-        return this._strokeColor || "red"
-    }
-    set strokeColor(color: string) {
-        this._strokeColor = color
-        /*
-            https://github.com/pixijs/pixijs/blob/v8.0.0-beta.11/src/scene/graphics/shared/utils/convertFillInputToFillStyle.ts#L92
-        */
-        if (this._strokeBrush) {
-            this._strokeBrush.strokeStyle.color = Color.shared.setValue(color).toNumber()
-        }
-    }
+    strokeColor = "red"
+    strokeThickness = 1
+    brushTransparency = 0
 
-    _strokeThickness?: number
-    get strokeThickness() {
-        return this._strokeThickness || 1
-    }
-    set strokeThickness(n: number) {
-        this._strokeThickness = n
-        if (this._strokeBrush) {
-            this._strokeBrush.strokeStyle.width = n
-        }
-    }
-    
-    _brushTransparency?: number
-    get brushTransparency() {
-        return this._brushTransparency || 0
-    }
-    set brushTransparency(n: number) {
-        this._brushTransparency = n
-        if (this._strokeBrush) {
-            this._strokeBrush.alpha = 1 - n / 100
-        }
-    }
     override addSibling(
         project: Module,
         target: Container,
