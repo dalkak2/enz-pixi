f80d955d3a4ceed3dca23d9f14bd8e8026021bc4
Gnlow <gnlow@pm.me>
2025-09-02 06:59:23 +0900

    refactor: Variable._get, ._set

--- src/block/Variable.ts
+++ src/block/Variable.ts
@@ -18,15 +18,22 @@ export class Variable extends Module {
             )
         )
     }
-    
-    get_variable(id: string) {
+
+    _get(id: string) {
         return this.variables[id]
     }
+    _set(id: string, value: string | number | (string | number)[]) {
+        this.variables[id] = value
+    }
+    
+    get_variable(id: string) {
+        return this._get(id)
+    }
     change_variable(id: string, value: number) {
-        this.variables[id] = Number(this.variables[id]) + Number(value)
+        this._set(id, Number(this._get(id)) + Number(value))
     }
     set_variable(id: string, value: string | number) {
-        this.variables[id] = value
+        this._set(id, value)
     }
     show_variable() {
         console.log("skip:", "show_variable")
@@ -38,51 +45,51 @@ export class Variable extends Module {
         if (!Array.isArray(this.variables[id])) {
             throw new Error(`${id} is not array`)
         }
-        return (this.variables[id] as (string | number)[])[i - 1]
+        return (this._get(id) as (string | number)[])[i - 1]
     }
     add_value_to_list(value: string | number, id: string) {
-        if (!Array.isArray(this.variables[id])) {
+        if (!Array.isArray(this._get(id))) {
             throw new Error(`${id} is not array`)
         }
-        return (this.variables[id] as (string | number)[]).push(value)
+        return (this._get(id) as (string | number)[]).push(value)
     }
     remove_value_from_list(i: number, id: string) {
-        if (!Array.isArray(this.variables[id])) {
+        if (!Array.isArray(this._get(id))) {
             throw new Error(`${id} is not array`)
         }
-        (this.variables[id] as (string | number)[]).splice(i - 1, 1)
+        (this._get(id) as (string | number)[]).splice(i - 1, 1)
     }
     insert_value_to_list(
         value: string | number,
         id: string,
         i: number,
     ) {
-        if (!Array.isArray(this.variables[id])) {
+        if (!Array.isArray(this._get(id))) {
             throw new Error(`${id} is not array`)
         }
-        (this.variables[id] as (string | number)[]).splice(i - 1, 0, value)
+        (this._get(id) as (string | number)[]).splice(i - 1, 0, value)
     }
     change_value_list_index(
         id: string,
         i: number,
         value: string | number,
     ) {
-        if (!Array.isArray(this.variables[id])) {
+        if (!Array.isArray(this._get(id))) {
             throw new Error(`${id} is not array`)
         }
-        (this.variables[id] as (string | number)[])[i - 1] = value
+        (this._get(id) as (string | number)[])[i - 1] = value
     }
     length_of_list(id: string) {
-        if (!Array.isArray(this.variables[id])) {
+        if (!Array.isArray(this._get(id))) {
             throw new Error(`${id} is not array`)
         }
-        return (this.variables[id] as (string | number)[]).length
+        return (this._get(id) as (string | number)[]).length
     }
     is_included_in_list(id: string, value: string | number) {
-        if (!Array.isArray(this.variables[id])) {
+        if (!Array.isArray(this._get(id))) {
             throw new Error(`${id} is not array`)
         }
-        return (this.variables[id] as (string | number)[]).includes(value)
+        return (this._get(id) as (string | number)[]).includes(value)
     }
     show_list() {
         console.log("skip:", "show_list")
