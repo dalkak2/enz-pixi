f8d5dc407a44b3747d11a0f2aa705455924e2f03
Gnlow <gnlow@pm.me>
2025-09-05 21:51:24 +0900

    fix: build to es2024 for browser

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,10 +1,11 @@
-deno run -A npm:esbuild@0.25.4 src/mod.ts \
+deno run -A npm:esbuild@0.25.9 src/mod.ts \
     --bundle \
     --outfile=dist/src/mod.ts \
     --format=esm \
     --minify \
+    --target=es2024 \
 
-deno run -A npm:esbuild@0.25.4 api/mod.ts \
+deno run -A npm:esbuild@0.25.9 api/mod.ts \
     --bundle \
     --outfile=dist/api/mod.ts \
     --format=esm \
